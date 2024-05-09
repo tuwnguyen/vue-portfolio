94d9858b2ff7112ba867d820c190f0825e84d45c
tuwnguyen <tuwnguyen@gmail.com>
2024-05-09 15:09:05 +0700

    imp Timeline component

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -5,9 +5,9 @@
         <span class="text-3xl font-medium dark:text-[#cac9c9]">about me.</span>
       </div>
       <hr width="50%" />
-      <div class="flex flex-auto flex-col items-center justify-center gap-4 p-4 md:flex-row">
-        <div class="w-full md:w-1/2"><Timeline /></div>
-        <div class="w-full md:w-1/2"><Timeline /></div>
+      <div class="flex flex-col gap-4 p-4 md:flex-row">
+        <div class="w-full md:w-1/2"><Timeline :data="education" /></div>
+        <div class="w-full md:w-1/2"><Timeline :data="experience" /></div>
       </div>
     </div>
   </div>
@@ -15,6 +15,15 @@
 
 <script setup>
 import Timeline from './helpers/Timeline.vue'
+import info from '../../info'
+const education = {
+  title: 'education',
+  data: info.education
+}
+const experience = {
+  title: 'experience',
+  data: info.experience
+}
 </script>
 
 <style lang="scss" scoped></style>
--- src/components/helpers/Timeline.vue
+++ src/components/helpers/Timeline.vue
@@ -1,69 +1,40 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <ol class="relative border-s border-gray-200 dark:border-gray-700">
+  <p class="my-4 text-2xl font-medium dark:text-[#cac9c9]">{{ data.data.title }}</p>
+  <ol
+    v-for="(e, idx) in data.data.data"
+    :key="idx"
+    class="relative border-s border-gray-200 dark:border-gray-700"
+  >
     <li class="mb-10 ms-4">
       <div
         class="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"
       ></div>
-      <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-        >February 2022</time
-      >
-      <h3 class="text-lg font-medium dark:text-[#cac9c9]">Application UI code in Tailwind CSS</h3>
+      <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{{
+        e.date
+      }}</time>
+      <h3 class="text-lg font-medium dark:text-[#cac9c9]">{{ e.name }}, {{ e.place }}</h3>
+      <p class="mb-4 text-base text-gray-500 dark:text-gray-400">{{ e.degree || e.position }}</p>
       <p class="mb-4 text-base text-gray-500 dark:text-gray-400">
-        Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar,
-        and pre-order E-commerce & Marketing pages.
+        {{ e.description }}
       </p>
-      <a
-        href="#"
-        class="inline-flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
-        >Learn more
-        <svg
-          class="ms-2 h-3 w-3 rtl:rotate-180"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 5h12m0 0L9 1m4 4L9 9"
-          /></svg
-      ></a>
-    </li>
-    <li class="mb-10 ms-4">
-      <div
-        class="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"
-      ></div>
-      <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-        >March 2022</time
+      <span
+        v-for="sk in e.skills"
+        :key="sk"
+        class="me-2 inline-block rounded border border-gray-500 bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-400"
+        >{{ sk }}</span
       >
-      <h3 class="text-lg font-medium dark:text-[#cac9c9]">Marketing UI design in Figma</h3>
-      <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-        All of the pages and components are first designed in Figma and we keep a parity between the
-        two versions even as we update the project.
-      </p>
-    </li>
-    <li class="ms-4">
-      <div
-        class="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"
-      ></div>
-      <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-        >April 2022</time
-      >
-      <h3 class="text-lg font-medium text-gray-900 dark:text-[#cac9c9]">
-        E-Commerce UI code in Tailwind CSS
-      </h3>
-      <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-        Get started with dozens of web components and interactive elements built on top of Tailwind
-        CSS.
-      </p>
     </li>
   </ol>
 </template>
-
-<script setup></script>
+<script setup>
+const data = defineProps({
+  data: {
+    title: String,
+    data: Object
+  }
+})
+console.log('data', data.data)
+</script>
 
 <style lang="scss" scoped></style>
