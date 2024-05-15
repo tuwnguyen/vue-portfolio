0e421dbc2282b97f79e4f88286b54cf31d5e333e
tuwnguyen <tuwnguyen@gmail.com>
2024-05-15 22:56:47 +0700

    install and add config file emailjs

---
+++ config.js
@@ -0,0 +1,10 @@
+let config = {
+  emailjs: {
+    serviceID: 'service_7zg6ghl',
+    templateID: 'template_wnqs8ot',
+    publicKey: '8i7rVGrgoYEhjfP2g',
+    toName: 'huynguyen'
+  }
+}
+
+export default config
